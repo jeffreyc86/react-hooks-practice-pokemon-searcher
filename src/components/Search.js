0d3b061d4aa942f88699e280da6dc9501f3ef750
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-07 08:49:14 -0500

    commit tree d4e0aa688c5a3ae9090983d05f237b5241669d9c

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Search() {
+function Search({query, setQuery}) {
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input className="prompt" value={query} onChange={(e)=>setQuery(e.target.value)} />
         <i className="search icon" />
       </div>
     </div>
